e87a97f420c0b8b60232b62b4f6804f050f28bcc
Grayson Gayle <Grayson@wireless-10-147-28-55.public.utexas.edu>
2015-11-13 15:16:33 -0600

    .twb, png images, and rmd

--- Scatterplot.R
+++ Scatterplot.R
@@ -8,15 +8,12 @@ require(dplyr)
 # The following is equivalent to "04 Blending 2 Data Sources.twb"
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-                                                """
-                                                select  b.measure_name as measure_names, a.profit || \\\' \\\' || b.raw_value as measure_values
+                                                """select   b.measure_name as measure_names, a.profit || \\\' \\\' || b.raw_value as measure_values
                                                 from superstore a inner join countyhealth b on a.state_or_province = b.state
-                                                where b.measure_name = \\\'Violent crime rate\\\' and a.profit > \\\'0 \\\'
-                                                order by a.profit desc
-                                                """
+                                                where b.measure_name = \\\'Violent crime rate\\\' and a.profit > \\\'0\\\' and rownum < 15000"""
                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gmg954', PASS='orcl_gmg954', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))); 
 
-df
+View(df)
 
 ggplot() + 
   coord_cartesian() + 
@@ -24,22 +21,17 @@ ggplot() +
   #scale_y_continuous() +
   #facet_wrap(~CLARITY, ncol=1) +
   labs(title='Blending 2 Data Sources') +
-  labs(x=paste("State"), y=paste("Avg(Raw_Value")) +
+  labs(x=paste("Profit"), y=paste("Raw Value")) +
   layer(data=df, 
         mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES), 
         stat="identity", 
         stat_params=list(), 
-        geom="bar",
+        geom="point",
         geom_params=list(colour=NA), 
         position=position_identity()
-  ) + coord_flip() +
-  layer(data=df, 
-        mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES, label=(MEASURE_VALUES)), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="text",
-        geom_params=list(colour="black", hjust=-0.5), 
-        position=position_identity()) 
+  ) 
+  
+
 
 
 
